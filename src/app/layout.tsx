3f31406e02ae3bb7af8f531c3438f7d089c03db3
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-10-13 20:23:49 +0200

    test deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,5 @@
-"use client"
-
 import './globals.css';
 import type { Metadata } from 'next';
-import { DataProvider } from './contexts/data';
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
 
 export const metadata: Metadata = {
   title: 'Fl0jfk',
@@ -18,13 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
-      <DataProvider>
         <body style={{backgroundImage: "linear-gradient(to right, #434343 0%, black 100%)"}} className="flex flex-col w-screen text-white">  
-            <Header/>
               {children}
-            <Footer/> 
         </body>
-      </DataProvider>  
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Banner from "./components/banner/Banner";
 import Skill from './components/skill/Skill';
 import Project from './components/project/Project';
@@ -7,6 +9,9 @@ import Experience from './components/experience/Experience';
 import Testimonial from './components/testimonial/Testimonial';
 import Contact from './components/contact/Contact';
 import ParticlesBG from './components/particles/Particles';
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import { DataProvider } from './contexts/data';
 
 function Home() {
   return (
@@ -14,16 +19,20 @@ function Home() {
       <Head>
         <title>Page d`&apos;`accueil Flojfk</title>
       </Head>
-      <main>  
-          <ParticlesBG/>
-          <Banner/>
-          <About/>
-          <Skill/>
-          <Project/>
-          <Experience/>
-          <Testimonial/>
-          <Contact/>
-      </main>
+      <DataProvider>
+        <Header/>
+        <main>  
+            <ParticlesBG/>
+            <Banner/>
+            <About/>
+            <Skill/>
+            <Project/>
+            <Experience/>
+            <Testimonial/>
+            <Contact/>
+        </main>
+        <Footer/> 
+      </DataProvider>  
     </> 
   )
 }
