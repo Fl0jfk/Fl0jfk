a7142a7217e3ff9d77717dfe5cfcb2fa11e11547
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-10-17 19:35:12 +0200

    disable ui gm

--- src/app/components/map/Map.tsx
+++ src/app/components/map/Map.tsx
@@ -12,6 +12,7 @@ export default function Map() {
   function MapKit(){
     const center = useMemo(() => ({ lat: 49.408276, lng: 1.240513 }), []);
     const data = useData();
+    
   return (
     <GoogleMap zoom={10} center={center} mapContainerClassName="max-w-[800px] w-full h-[400px] rounded-xl mx-auto" options={{ disableDefaultUI: true }}>
         {data.profile.memoji&&<Marker position={center} icon={{url: data.profile.memoji, scaledSize: new window.google.maps.Size(80, 80),}}/>}
