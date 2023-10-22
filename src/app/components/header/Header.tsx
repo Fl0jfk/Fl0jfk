17eaa811fc83e8b325ebe76f6fed32c1dcbe48d1
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-10-22 17:32:17 +0200

    refresh menu

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -6,15 +6,16 @@ import Image from 'next/image';
 import { useData } from '../../contexts/data';
 import CrossButton from '../buttons/CrossButton';
 import { motion, useScroll, useMotionValueEvent } from 'framer-motion';	
-import ClickAwayListener from 'react-click-away-listener';
 
 function Header(){
     const data = useData();
     const { scrollY } = useScroll();
     const [menuOpened, setMenuOpened] = useState(false);
     const [hidden, setHidden ] = useState(false);
+    const opacityMenu = (!menuOpened? "sm:opacity-80 md:opacity-80" : "")
     const handleClick = () => {
-        setMenuOpened(!menuOpened)
+        setMenuOpened(!menuOpened);
+        
     };
    useMotionValueEvent(scrollY, "change", (latest) => {
     const previous = scrollY.getPrevious();
@@ -24,32 +25,27 @@ function Header(){
         setHidden(false);
     }
     });
-    const handleClickAway = () => {
-        setMenuOpened(false)
-    }
     const handleLinkClick = ({ clickOnLink } : handleLinkClickProps) => {
         setMenuOpened(clickOnLink);
     };
     return (
-        <ClickAwayListener onClickAway={() => {handleClickAway()}}>
             <motion.header 
                 variants={{ visible: { y: 0 }, hidden: { y: "-100%" }}} 
                 animate={hidden ? "hidden" : "visible"}
                 transition={{duration: 0.35, ease: "easeInOut"}}
-                className="flex p-4 justify-between w-full md:fixed sm:fixed z-[12] md:mb-[100px] sm:opacity-80 md:opacity-80 md:bg-[#000000] sm:bg-[#000000]">
+                className={`flex p-4 justify-between w-full md:fixed sm:fixed z-[12] md:mb-[100px] md:bg-[#000000] sm:bg-[#000000] ${opacityMenu}`}>
                     <div className='w-2/12 h-[100px] md:h-[50px] sm:h-[30px] flex items-center pt-4'>
                         {data.profile.memoji && 
                             <Image src={data.profile.memoji} alt='Mon memoji' width={100} height={100} className='cursor-pointer z-[8]' onClick={()=>{window.scrollTo({top:0, left:0, behavior:'smooth'})}}/>
                         }
                     </div>
                     <div className='w-10/12 flex justify-end items-center sm:mt-[-5px]'>
-                        <Navbar menuOpened={menuOpened} onLinkClick={handleLinkClick} />
+                        <Navbar menuOpened={menuOpened} onLinkClick={handleLinkClick}/>
                         <div className='flex justify-end w-[40] h-[100px] md:h-[50px] sm:h-[30px] items-center' onClick={() => handleClick()}>
                             <CrossButton menuOpened={menuOpened}/>
                         </div>
                     </div>
             </motion.header>
-        </ClickAwayListener>
     )
 }
 
