9bb2a0a5ae65cdee4f466fde01c17d117e2dc56d
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-10-23 11:15:42 +0200

    restyle projects section

--- src/app/components/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return (
-        <footer className="p-4">
-            <p className="text-xl text-center">&copy; 2023 Fl0jfk</p> 
-        </footer>
-    )
-}
-
-export default Footer;
