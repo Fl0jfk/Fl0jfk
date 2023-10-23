a5fe66c313c713a733a0284c807d93599b2d6d15
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-10-23 15:15:56 +0200

    add aria to links

--- src/app/components/buttons/LinkButton.tsx
+++ src/app/components/buttons/LinkButton.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 function LinkButton ({text, full , link} : ButtonProps){
     const borderStyle = (full ? "bg-green text-grey border-grey" : "text-green hover:scale-110 border-green" );
  return (
-        <button className={`${borderStyle} rounded border-2 p-2 text-xl`} type="button" aria-label="Link to section Contact">
-            <Link href={link}>
+        <button className={`${borderStyle} rounded border-2 p-2 text-xl`} type="button" aria-label="Link">
+            <Link href={link} aria-label="Link to section Contact">
                 {text}
             </Link>
         </button>
