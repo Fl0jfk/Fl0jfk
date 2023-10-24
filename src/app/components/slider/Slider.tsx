d19f75b078fdf18dbaffb83c0d9a867bf7a3084c
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-10-24 11:58:32 +0200

    add drag fluid

--- src/app/components/slider/Slider.tsx
+++ src/app/components/slider/Slider.tsx
@@ -1,47 +1,58 @@
-import Slide from "./Slide";
 import React, { useState, useRef } from "react";
+import Slide from "./Slide";
 
 function Slider({ props }: SliderProps) {
-  const containerRef = useRef<HTMLDivElement | null>(null); 
+  const containerRef = useRef<HTMLDivElement | null>(null);
   const [isDragging, setIsDragging] = useState(false);
   const [startX, setStartX] = useState(0);
   const [scrollLeft, setScrollLeft] = useState(0);
+
   const handleMouseDown = (e: React.MouseEvent) => {
     if (containerRef.current) {
       setIsDragging(true);
-      setStartX(e.pageX - containerRef.current.getBoundingClientRect().left);
+      setStartX(e.pageX);
       setScrollLeft(containerRef.current.scrollLeft);
-      containerRef.current.classList.add("no-select");
     }
   };
+
   const handleMouseUp = () => {
     setIsDragging(false);
-    if (containerRef.current) {
-      containerRef.current.classList.remove("no-select");
-    }
   };
+
   const handleMouseMove = (e: React.MouseEvent) => {
     if (!isDragging || !containerRef.current) return;
-    const x = e.pageX - containerRef.current.getBoundingClientRect().left;
-    const walk = (x - startX) * 2;
-    containerRef.current.scrollLeft = scrollLeft - walk;
+    const x = e.pageX;
+    const diffX = x - startX;
+    const newScrollLeft = scrollLeft - diffX;
+    containerRef.current.scrollLeft = newScrollLeft;
   };
+
   return (
     <div
-    ref={containerRef}
-    className="flex items-center overflow-x-scroll snap-x snap-mandatory cursor-grag"
-    onMouseDown={handleMouseDown}
-    onMouseUp={handleMouseUp}
-    onMouseLeave={handleMouseUp}
-    onMouseMove={handleMouseMove}
-  >
-            {props.map((testimonial) => {
-              return <Slide author={testimonial.author} key={testimonial.id} job={testimonial.job} photoAuthor={testimonial.photoAuthor} testimonial={testimonial.testimonial} website={testimonial.website} linkedin={testimonial.linkedin}/>;
-            })}
-          </div>
+      ref={containerRef}
+      className="flex items-center overflow-x-scroll snap-x snap-mandatory cursor-grab select-none"
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
+      onMouseMove={handleMouseMove}
+    >
+      {props.map((testimonial) => {
+        return (
+          <Slide
+            author={testimonial.author}
+            key={testimonial.id}
+            job={testimonial.job}
+            photoAuthor={testimonial.photoAuthor}
+            testimonial={testimonial.testimonial}
+            website={testimonial.website}
+            linkedin={testimonial.linkedin}
+          />
+        );
+      })}
+    </div>
   );
-};
-  
+}
+
 type SliderProps = {
   props: any[];
 };
