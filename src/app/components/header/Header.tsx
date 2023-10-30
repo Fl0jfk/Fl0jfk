aae5595d1cc3fec849293deec8c4a2c1b5648ba4
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-10-30 16:47:36 +0100

    fix header add max width n self center

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -32,7 +32,7 @@ function Header(){
             variants={{ visible: { y: 0 }, hidden: { y: "-100%" }}} 
             animate={hidden ? "hidden" : "visible"}
             transition={{duration: 0.35, ease: "easeInOut"}}
-            className={`flex p-4 justify-between w-full md:fixed sm:fixed z-[12] md:mb-[100px] md:bg-[#000000] sm:bg-[#000000] ${opacityMenu}`}>
+            className={`flex p-4 justify-between w-full md:fixed sm:fixed z-[12] md:mb-[100px] md:bg-[#000000] sm:bg-[#000000] ${opacityMenu} max-w-[1440px] self-center`}>
                 <div className='w-2/12 h-[100px] md:h-[50px] sm:h-[30px] flex items-center pt-4'>
                     {data.profile.memoji && 
                         <Image src={data.profile.memoji} alt='Mon memoji' width={100} height={100} className='cursor-pointer z-[8]' onClick={()=>{window.scrollTo({top:0, left:0, behavior:'smooth'})}}/>
